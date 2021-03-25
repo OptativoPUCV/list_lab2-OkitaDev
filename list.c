676d1b1295944f47b94372028fef7718461239d2
Replit user <replituser@example.com>
2021-03-25 18:42:35 +0000

    new try

--- list.c
+++ list.c
@@ -49,7 +49,7 @@ void * firstList(List * list)
 void * nextList(List * list) 
 {
 	list->current = list->current->next;
-	if(list->current != NULL || list->current->next != NULL) return (void *) list->current->data;
+	if(list->current != NULL && list->current->next != NULL) return (void *) list->current->data;
 
 	return NULL;
 }
